f62da9a20bf6756b2792201cacbc030aae0a427b
Le Bao <lhqbao@gmail.com>
2021-09-30 17:10:47 +0700

    //hello into Form1

--- Salary/Form1.cs
+++ Salary/Form1.cs
@@ -16,5 +16,10 @@ namespace Salary
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // hello
+        }
     }
 }
